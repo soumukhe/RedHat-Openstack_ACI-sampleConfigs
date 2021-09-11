c49ded46775085ec018876568194d33326a52900
soumukhe <soumukhe@cisco.com>
2021-09-11 09:12:38 -0500

    update

---
+++ home/stack/templates/deploy.sh.mtu1500-ACI.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+source ~/stackrc
+
+time openstack overcloud deploy --templates \
+-r  /home/stack/templates/aci_roles_data.yaml \
+-e /home/stack/templates/node-info.yaml \
+-n /home/stack/templates/network_data.yaml \
+-e /home/stack/templates/overcloud_images.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
+-e  /home/stack/templates/ciscoaci.yaml \
+-e /home/stack/templates/ciscoaci_containers.yaml \
+--ntp-server 192.168.24.20 \
+--debug
+
---
+++ home/stack/templates/deploy.sh.mtu1500-noACI.sh
@@ -0,0 +1,11 @@
+time openstack overcloud deploy --templates \
+-r  /home/stack/templates/roles_data.yaml \
+-e /home/stack/templates/node-info.yaml \
+-e /home/stack/templates/overcloud_images.yaml \
+-n /home/stack/templates/network_data.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
+-e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
+--ntp-server 192.168.24.20 \
+--debug
+
