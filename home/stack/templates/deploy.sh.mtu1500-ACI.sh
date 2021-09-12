45fe4eb1e0eb5162e71aefe89b235bc77a161f5f
soumukhe <soumukhe@cisco.com>
2021-09-12 12:53:01 -0500

    updated

--- home/stack/templates/deploy.sh.mtu1500-ACI.sh
+++ home/stack/templates/deploy.sh.mtu1500-ACI.sh
@@ -10,7 +10,7 @@ time openstack overcloud deploy --templates \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/network-environment.yaml \
 -e /usr/share/openstack-tripleo-heat-templates/environments/net-single-nic-with-vlans.yaml \
--e  /home/stack/templates/ciscoaci.yaml \
+-e  /home/stack/templates/ciscoaci.1500.yaml \
 -e /home/stack/templates/ciscoaci_containers.yaml \
 --ntp-server 192.168.24.20 \
 --debug
